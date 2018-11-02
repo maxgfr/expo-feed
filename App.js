9fc3b50c1d719ef0660a052fb4ebb2ed6041aee1
Maxime Golfier <maxgolfier@gmail.com>
2018-11-02 19:09:50 +0100

    maxime_commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView style={styles.container}>
+      </ScrollView>
     );
   }
 }
@@ -14,8 +13,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
